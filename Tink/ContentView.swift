03a8be92b56f62c54f3e756adf2dd1e6aaa76bc7
pablofu9 <pablofuertesruiz@gmail.com>
2025-02-07 10:40:50 +0100

    Initial Commit

---
+++ Tink/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tink
+//
+//  Created by Pablo Fuertes ruiz on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tink/TinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinkApp.swift
+//  Tink
+//
+//  Created by Pablo Fuertes ruiz on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
